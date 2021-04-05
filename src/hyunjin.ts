c6d55f29206987b8a3d47ba5795022de6e47305a
Chehui Chou <deadshot465@gmail.com>
2021-04-05 12:19:52 +0900

    Add presences update.

--- src/hyunjin.ts
+++ src/hyunjin.ts
@@ -1,10 +1,16 @@
 import { CommandClient } from 'eris';
+import { PRESENCES } from './constants';
 import config from './config';
 import commands from './commands/index';
+import getRandomNumber from './utilities/getRandomNumber';
 import messageHandlers from './messageHandlers/index';
 
 const hyunjin = new CommandClient(config.botToken, {}, {
     prefix: config.prefix,
+});
+
+hyunjin.on('ready', () => {
+    updateStatus();
 });
 
 hyunjin.on('messageCreate', (msg) => {
@@ -21,3 +27,11 @@ commands.forEach((e) => {
 hyunjin.connect().then(async () => {
     console.log(`Hyunjin is now online with the prefix ${config.prefix}`);
 });
+
+function updateStatus() {
+    hyunjin.editStatus('online', {
+        name: PRESENCES[getRandomNumber(PRESENCES.length)],
+        type: 0
+    });
+    setTimeout(updateStatus, 3600 * 1000);
+}
