710ca18a9558e2119e0d034d6fe583c850a196db
not-archiey <notarchiey@protonmail.com>
2021-04-05 08:27:04 +0700

    fix: suppress eslint import/prefer-default-export

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 const HYUNJIN_COLOR = 0x43729F;
 
+// Remove when multiple constants has been exported from this file
+// eslint-disable-next-line import/prefer-default-export
 export { HYUNJIN_COLOR };
